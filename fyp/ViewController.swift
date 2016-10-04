9697cb465f230e27fc407bedcb3d8b47e4fe2958
Ka Hong Ngai <kahong@Kas-MacBook-Air.local>
2016-10-04 19:17:56 +0800

    commit files

---
+++ fyp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fyp
+//
+//  Created by Ka Hong Ngai on 4/10/2016.
+//  Copyright © 2016 IK1603. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ fypTests/fypTests.swift
@@ -0,0 +1,36 @@
+//
+//  fypTests.swift
+//  fypTests
+//
+//  Created by Ka Hong Ngai on 4/10/2016.
+//  Copyright © 2016 IK1603. All rights reserved.
+//
+
+import XCTest
+@testable import fyp
+
+class fypTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
